6a4845ba2f63233177db7cad829508d04160e7f8
Ellen Spertus <spertus@mills.edu>
2020-10-26 10:16:43 -0700

    Use fluent and binding APIs

--- src/propertydemo/Main.java
+++ src/propertydemo/Main.java
@@ -10,7 +10,9 @@ import javafx.beans.property.SimpleIntegerProperty;
 public class Main {
     private static IntegerProperty num1 = new SimpleIntegerProperty(1);
     private static IntegerProperty num2 = new SimpleIntegerProperty(2);
-    private static NumberBinding sum = Bindings.add(num1, num2);
+    private static IntegerProperty num3 = new SimpleIntegerProperty(3);
+    private static IntegerProperty num4 = new SimpleIntegerProperty(4);
+    private static NumberBinding sum = Bindings.add(num1.multiply(num2), num3.multiply(num4));
 
     public static void main(String[] args) {
         showValues();
@@ -22,7 +24,8 @@ public class Main {
     }
 
     private static void showValues() {
-        System.out.println("num1: " + num1.getValue() + ", num2: " + num2.getValue() + ", sum: "
-                + sum.getValue());
+        System.out.println("num1: " + num1.getValue() + ", num2: " + num2.getValue() +
+                ", num3: " + num3.getValue() + ", num4: " + num4.getValue() +
+                ", sum: " + sum.getValue());
     }
 }
