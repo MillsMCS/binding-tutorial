8fe061e0586b000e37383627995c5df15226354e
Ellen Spertus <spertus@mills.edu>
2020-10-26 10:09:05 -0700

    Use the fluent API

--- src/propertydemo/Main.java
+++ src/propertydemo/Main.java
@@ -1,18 +1,27 @@
 package propertydemo;
 
 import java.util.Scanner;
+import javafx.beans.binding.NumberBinding;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 
 // https://docs.oracle.com/javafx/2/binding/jfxpub-binding.htm
 public class Main {
+    private static IntegerProperty num1 = new SimpleIntegerProperty(1);
+    private static IntegerProperty num2 = new SimpleIntegerProperty(2);
+    private static NumberBinding sum = num1.add(num2);
+
     public static void main(String[] args) {
-        Bill electricBill = new Bill();
-
-        electricBill.amountDueProperty().addListener(
-                (o, oldVal, newVal) -> System.out.println("Electric bill has changed!"));
-
+        showValues();
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Enter amount due: ");
-            electricBill.setAmountDue(scanner.nextDouble());
+            System.out.println("Enter new value for num1: ");
+            num1.set(scanner.nextInt());
         }
+        showValues();
+    }
+
+    private static void showValues() {
+        System.out.println("num1: " + num1.getValue() + ", num2: " + num2.getValue() + ", sum: "
+                + sum.getValue());
     }
 }
