9e1d24b1cf23140770c6e56d0ec80d8f66c6845a
Ellen Spertus <spertus@mills.edu>
2022-04-04 18:19:18 -0700

    Replace tabs with spaces

--- src/propertydemo/Main.java
+++ src/propertydemo/Main.java
@@ -1,24 +1,23 @@
 package propertydemo;
 
 import java.util.Scanner;
-
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 
 // https://docs.oracle.com/javafx/2/binding/jfxpub-binding.htm
 public class Main {
-	public static void main(String[] args) {
-		Bill electricBill = new Bill();
+    public static void main(String[] args) {
+        Bill electricBill = new Bill();
 
-		electricBill.amountDueProperty().addListener(new ChangeListener() {
-			@Override
-			public void changed(ObservableValue o, Object oldVal, Object newVal) {
-				System.out.println("Electric bill has changed!");
-			}
-		});
-		try (Scanner scanner = new Scanner(System.in)) {
-			System.out.println("Enter amount due: ");
-			electricBill.setAmountDue(scanner.nextDouble());
-		}
-	}
+        electricBill.amountDueProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue o, Object oldVal, Object newVal) {
+                System.out.println("Electric bill has changed!");
+            }
+        });
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter amount due: ");
+            electricBill.setAmountDue(scanner.nextDouble());
+        }
+    }
 }
